3f84812895bb638c43a0dbe55c4369f498a74891
Daniel Horowitz <danielhorowitzzz@gmail.com>
2019-03-28 00:14:40 +0100

    Update install bin

--- adb-anim.rb
+++ adb-anim.rb
@@ -8,7 +8,7 @@ class AdbAnim < Formula
   sha256 "00c9a854c2f7cb2bf6d9d0524bf6d52a0f8c0d16cba5f55d0a3bf5530a4030a4"
 
   def install
-    bin.install "adb-anim"
+    bin.install "adb-anim.sh"
   end
 
 end
